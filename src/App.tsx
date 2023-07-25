ef8c29d8f8c207916f4ea2d8bac93eb85a8d76ad
Maria Vitória Kunz <mvkunz.dev@gmail.com>
2023-07-25 16:21:02 -0300

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,52 +3,8 @@ import Title from './components/Title';
 import Footer from './components/Footer';
 import Book from './components/Book';
 
+import { booksList } from './data';
 import './App.css'
-
-const booksList = [
-    {
-      title: 'The fault in our stars',
-      pages: 288,
-      isRead: true,
-      isFavorite: false,
-    },
-    {
-      title: 'Fifty Shades of Grey',
-      pages: 480,
-      isRead: true,
-      isFavorite: false,
-    },
-    {
-      title: 'GREY',
-      pages: 528,
-      isRead: true,
-      isFavorite: true,
-    },
-    {
-      title: 'Harry Potter and the Philosophers Stone',
-      pages: 264,
-      isRead: true,
-      isFavorite: false,
-    },
-    {
-      title: 'Harry Potter and the Chamber Of Secrets',
-      pages: 224,
-      isRead: true,
-      isFavorite: true,
-    },
-    {
-      title: 'Harry Potter and the Half-Blood Prince',
-      pages: 512,
-      isRead: false,
-      isFavorite: false,
-    },
-    {
-      title: 'Harry Potter and the Deathly Hallows',
-      pages: 759,
-      isRead: false,
-      isFavorite: false,
-    },
-];
 
 function App() {
   return (
