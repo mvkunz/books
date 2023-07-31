e97e91f8f0da6b21c9dc41e762ad1428967a6361
Maria Vitória Kunz <mvkunz.dev@gmail.com>
2023-07-31 10:42:34 -0300

    validacao

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
   const [bookTitle, setBookTitle] = useState('');
   const [bookPages, setBookPages] = useState(0);
   const [books, setBooks] = useState<Booktype[]>([]);
+  const [showFormMessage, setShowFormMessage] = useState(false);
 
   function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
     setBookTitle(event.target.value);
@@ -40,10 +41,16 @@ function App() {
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    updateState();
-    resetForm();
+    if (bookTitle !== '' && bookPages > 0) {
+      updateState();
+      resetForm();
+      setShowFormMessage(false);
+    } else {
+      setShowFormMessage(true);
+    }
   }
 // handleSubmit-> Quando fizer o submit do form (botão adicionar), vai chamar a preventDefault para evitar o comportamento padrão do form (que é recarregar a página) e depois vai chamar a função updateState que vai atualizar o estado. Logo após chamar a updateState, chamará a função resetForm para APAGAR o que havia sido digitado no input.
+// É realizada uma VALIDAÇÃO para verificar se o title NÃO é vazio e se numero de paginas é maior que 0.
 
   return (
     <div className="app">
@@ -75,6 +82,11 @@ function App() {
               value={bookPages}
               onChange={ handlePagesChange }
             />
+            {showFormMessage && (
+              <div className="form-message">
+                <p>Todos os campos são obrigatórios!</p>
+              </div>
+            )}
             <Button>
               Adicionar
             </Button>
@@ -102,3 +114,11 @@ export default App
 // Quando um button está dentro do form, automaticamente vira um tipo submit. É a mesma coisa que colcoar type=submit.
 
 // O preventDefault impede que o comportamento padrão do evento seja realizado. No caso do form, ele impedirá que o form recarregue a página.
+
+//             {showFormMessage && (
+//  <div className="form-message">
+//  <p>Todos os campos são obrigatórios!</p>
+//</div>
+//)}
+// É necessária a criação de um estado para controlar a mensagem do campo. Para isso, deve fazer uma condicional para definir qnd deverá ser exibida.
+// Só deverá ser exibido a mensagem quando a validação falhar, então deve fazer um else no if do handleSubmit e mudar o estado. MAS, para a mensagem não aparecer, deve alterar no if tb.
